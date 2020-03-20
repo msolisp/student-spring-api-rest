31f3872a87b195b5cfa397674ad0f4c2ea7ce46b
Max <max.solis@gmail.com>
2020-03-20 18:25:49 -0300

    initial commit of api rest students

---
+++ src/main/java/com/student/spring/api/StudentSpringApiRestApplication.java
@@ -0,0 +1,23 @@
+package com.student.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class StudentSpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSpringApiRestApplication.class, args);
+	}
+	
+	@RestController
+	class HelloController {
+		@GetMapping("/")
+		String hello(){
+			return "Hello from App Engine !";
+	}	
+	}
+
+}
